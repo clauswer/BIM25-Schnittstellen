24e3a00135edebe0827ff3b1b4ba0a0c20feabf3
Claus Werner <clwe3117@th-wildau.de>
2026-05-21 00:08:55 +0200

    Marc21-XML wird geparst; es werden aber noch nicht alle Felder wiedergegeben

--- Aufgaben_2026-05-04/search_GBV_SRU.py
+++ Aufgaben_2026-05-04/search_GBV_SRU.py
@@ -19,6 +19,8 @@ license: MIT License
 
 import requests
 import sys
+import xml.etree.ElementTree as ET
+
 
 
 def search_gbv_sru(
@@ -54,8 +56,6 @@ def search_gbv_sru(
 
     gbv_sru_url = f"https://sru.k10plus.de/{database}?version=1.1&operation=searchRetrieve&query={searchField}%3D{searchValue}&maximumRecords={maximumRecords}&recordSchema={recordSchema}"
 
-    #http://sru.k10plus.de/opac-de-627?version=1.1&operation=searchRetrieve&query=pica.tit%3DMcCarthyism&maximumRecords=10&recordSchema=mods
-
     try:
         response = requests.get(gbv_sru_url)
     except requests.exceptions as e:
@@ -64,19 +64,80 @@ def search_gbv_sru(
 
     return response.text
 
+
+# TODO: Funktion als Methode einer MARC21Record-Klasse implementieren, um die Notlösung mit den Tupeln für Marcxml-Record und Namespaces zu vermeiden; ist nicht sehr übersichtlich, aber für die Zwecke dieses Programms ausreichend
+# -> zuerst eine Methode, um einen string zu einem MARC21-XML-Record zu parsen; muss nichts aufregends sein, könnte aber später für Fehlerbehandlung nützlich sein, z. B. um ungültige XML-Strings abzufangen
+
+def parse_marcxml(marcxml_string:str) -> tuple[ET.Element, dict]:
+    """Parst einen MARC21-XML-String und gibt das Wurzelelement zurück.
+
+    Args:
+        marcxml_string (str): Ein String im MARC21-XML-Format
+    Returns:
+        Tuple aus:
+            ET.Element: Das Wurzelelement des geparsten MARC21-XML-Strings
+            dict: Ein Dictionary mit den definierten Namespaces für die Suche in den XML-Elementen
+    """
+    try:
+        root = ET.fromstring(marcxml_string)
+        # Definiere die Namespaces
+        namespaces = {
+            'zs': 'http://www.loc.gov/zing/srw/',
+            'marc': 'http://www.loc.gov/MARC21/slim'
+        }
+        return (root, namespaces)
+    
+    except ET.ParseError as e:
+        print(f"Fehler beim Parsen des MARC21-XML-Strings: {e}")
+        sys.exit(1)
+
+
+def get_field_from_marcxml(marc_tuple:tuple, tag:str, code:str) -> list[str]:
+    """Extrahiert die Werte eines bestimmten Feldes aus einem MARC21-XML-Record.
+
+    Args:
+        marc_tuple (tuple): Ein Tuple aus einem MARC21-XML-Record als ElementTree-Element und einem Dictionary mit den definierten Namespaces für die Suche in den XML-Elementen
+        tag (str): tag-Attribut für datafield-Element, z. B. 100 für Autor, 245 für Titel, 650 für Schlagworte
+        code (str): code-Attribut für subfield-Element
+
+    Returns:
+        list[str]: Eine Liste von Werten des angegebenen Feldes
+    """
+
+    marcxml = marc_tuple[0]
+    namespaces = marc_tuple[1]
+
+    values = []
+    for datafield in marcxml.findall(f".//marc:datafield[@tag='{tag}']", namespaces=namespaces):
+        for subfield in datafield.findall(f"marc:subfield[@code='{code}']", namespaces=namespaces):
+            values.append(subfield.text)
+    return values
+
+
 # TODO:
-# Parse Ergebnisse nach Titel -> Ausgabe der Felder je Titel
 # Main: starte Programm -> wonach suchen? -> Suchterme eingeben -> Suche durchführen -> Ergebnisse anzeigen
 
 
 if __name__ == "__main__":
 
-    print("Test der Funktion, maximumRecords = 1:\n\n")
 
-    for field, value in [("pica.tit","Python in a nutshell"), ("dc.author","Martelli, Alex"), ("pica.isb","0-596-10046-9")]:
+    for field, value in [("pica.tit","Python in a nutshell"), ("dc.author","Martelli, Alex"), ("pica.isb","9798341653597")]:
         print(f"Suche nach {field}: {value}")
+        
         xml_string = search_gbv_sru(field, value, maximumRecords=1)
-        filename = "./tests/test_" + field.replace(".", "-") + ".xml"
-        with open(filename, "w") as f:
-            f.write(xml_string)
-        print(f"Datei gespeichert als {filename}\n")
+        
+        marc_tuple = parse_marcxml(xml_string)
+
+        marcxml = marc_tuple[0]
+        namespaces = marc_tuple[1]
+
+        for record in marcxml.findall(".//marc:record", namespaces=namespaces):
+            print(f"ISBN: {", ".join(get_field_from_marcxml(marc_tuple, "020", "a"))}")
+            print(f"Autor: {", ".join(get_field_from_marcxml(marc_tuple, "100", "a"))}")
+            print(f"Titel: {", ".join(get_field_from_marcxml(marc_tuple, "245", "a"))}")
+            print(f"Schlagworte: {", ".join(get_field_from_marcxml(marc_tuple, "650", "a"))}")
+            print("\n")
+
+            
+
+        
